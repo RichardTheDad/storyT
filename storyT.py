734aad7a531214c0c65ac1af735b5ccba6de6b7b
RichardTheDaf <Richard.a.penland@gmail.com>
2023-08-22 00:29:35 -0400

    version 0.1

--- storyT.py
+++ storyT.py
@@ -1,1 +1,10 @@
+print("Before the story begins fill out the questions and be sure to hit enter to submit them.")
 
+FamilyName = input("\nPick a family name:  ")
+HighElfName = input("\nEnter the name of your elf:  ")
+HighElfGender = input("\nEnter the gender of your elf:  ")
+KingdomName = input("\nEnter the name of your kingdom:  ")
+Sibling = input("\nDo you have a brother or a sister:  ")
+
+
+print(f"\nYou are a {HighElfGender} Elf aprt of the distinguished {FamilyName} family. Your name is {HighElfName} and your family is in a crisis your {Sibling} has run away from home.")
