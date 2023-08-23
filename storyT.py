2657e9a666b4d6178c55ae30e026a5392629ef8b
RichardTheDaf <Richard.a.penland@gmail.com>
2023-08-22 21:12:14 -0400

    ver 0.2

--- storyT.py
+++ storyT.py
@@ -16,6 +16,22 @@ while Choice1.lower() not in ["y", "n"]:
 if Choice1.lower() == "y":
     print(f"\nYou run twords your {Sibling} brandishing your {Weapon} and you slay the feral beasts. You grab your {Sibling} and head off. ")
 elif Choice1.lower() == "n":
-    print(f"\nTerrified of the beasts you run abandoning your {Sibling}. As you bolt tripping throughout the forest you hear the screams of your {Sibling} as thier flesh be torn from bone. ")
+    print(f"\nTerrified of the beasts you run abandoning your {Sibling}. As you bolt tripping throughout the forest you hear the screams of your {Sibling} as their  flesh be torn from the bone. ")
 
+#choice 2 good ending
+if Choice1.lower() == "y":
+    print(f"\nNow with your startled {Sibling} held tightly in your arms will you head home or will you stay within the forest captivated by its beauty.")
+Choice2 = input("head home y, say in the forest n:  ")
+while Choice2.lower() not in ["y","n"]:
+    Choice2 = input("invalid value enter y or n:  ")
+if Choice2.lower() =="y":
+    print(f"\nYou head home with your {Sibling} thinking how great it will be to finaly head home after the long day.")
 
+#choice 2 bad ending
+if Choice1.lower() == "n":
+    print(f"\nYou hang your head in shame at your cowardice. Will you head home or will you stay within the forest to ashamed to show your face to your parents.")
+Choice2 = input("head home y, say in the forest n:  ")
+while Choice2.lower() not in ["y","n"]:
+    Choice2 = input("invalade value enter y or n:  ")
+if Choice1.lower() == "n":
+    print(f"\nYou head home to accept your punishment from your parents with a heavy heart.")
